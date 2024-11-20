89f46d8a4300afca22ad85bab1baadec87cfdc52
sferratore.francescopaolo00@gmail.com <sferratore.francescopaolo00@gmail.com>
2024-11-20 16:30:04 +0100

    Second red phase of Issue #3

--- test/test_greenhouse.py
+++ test/test_greenhouse.py
@@ -43,10 +43,18 @@ class TestGreenhouse(TestCase):
 
     @patch.object(Seesaw, "moisture_read")
     @patch.object(GPIO, "output")
-    def test_sprinkler_should_be_turned_on(self, mock_sprinkler: Mock, mock_moisture_sensor: Mock):
+    def test_sprinkler_should_be_turned_on_when_below_375(self, mock_sprinkler: Mock, mock_moisture_sensor: Mock):
         mock_moisture_sensor.return_value = 374
         system = Greenhouse()
         system.manage_sprinkler()
         mock_sprinkler.assert_called_once_with(system.SPRINKLER_PIN, True)
         self.assertTrue(system.sprinkler_on)
 
+    @patch.object(Seesaw, "moisture_read")
+    @patch.object(GPIO, "output")
+    def test_sprinkler_should_be_turned_off_when_above_425(self, mock_sprinkler: Mock, mock_moisture_sensor: Mock):
+        mock_moisture_sensor.return_value = 426
+        system = Greenhouse()
+        system.manage_sprinkler()
+        mock_sprinkler.assert_called_once_with(system.SPRINKLER_PIN, False)
+        self.assertTrue(not system.sprinkler_on)
